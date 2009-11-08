36a8354adbdd0101808dca152bb5acd710698420
M. Auayan <sxtxixtxcxh@gmail.com>
2009-11-07 18:46:54 -0800

    initial checkin

---
+++ Support/bin/compass_validator.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+FILEPATH = ENV['TM_FILEPATH']
+PATHS = File.dirname(FILEPATH).split('/');
+#PROJECT_ROOT =  FILEPATH.split("/app/")[0]
+dir = ''
+PATHS.reverse.each do |path|
+  dir = PATHS[0,PATHS.index(path)+1].join('/')
+  unless dir.empty?
+    Dir.chdir( dir )
+    unless( Dir.glob('config.rb').empty? )
+      COMPASS_ROOT = dir
+      break
+    end
+  end
+end
+
+if !defined? COMPASS_ROOT 
+  COMPASS_ROOT = FILEPATH.split("/app/")[0]
+end
+
+UPDATE_COMMAND = "compass --update #{COMPASS_ROOT} 2>&1"
+
+result = `#{UPDATE_COMMAND}`
+puts result
