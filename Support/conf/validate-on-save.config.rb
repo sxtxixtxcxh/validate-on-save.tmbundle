77f636b19e850c74bc67f382a1c18c2b2455e526
Jim Myhrberg <contact@jimeh.me>
2009-11-08 16:47:13 +0200

    Initial update to commands

---
+++ Support/conf/validate-on-save.config.rb
@@ -0,0 +1,15 @@
+
+# Only notify on syntax error
+VOS_ONLY_ON_ERROR = true
+
+# Output validator information
+VOS_VALIDATOR_INFO = false
+
+# Use Textmate's tooltip notification
+VOS_TM_NOTIFY = true
+
+# Use Growl notifications
+VOS_GROWL = false
+
+# Path to growlnotify binary
+VOS_GROWL_BIN = "/usr/local/bin/growlnotify"
