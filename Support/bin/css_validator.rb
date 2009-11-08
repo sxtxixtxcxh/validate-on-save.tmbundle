99de86a24486f1a1e7e630c1c09b0f167c045bb4
M. Auayan <sxtxixtxcxh@gmail.com>
2009-11-08 06:37:39 -0800

    multiline stripping of valid css output

--- Support/bin/css_validator.rb
+++ Support/bin/css_validator.rb
@@ -4,6 +4,6 @@ SUPPORT  = ENV['TM_BUNDLE_SUPPORT']
 
 result = `java -jar #{SUPPORT.gsub(' ','\ ')}/bin/css/css-validator.jar file:#{FILEPATH}`
 
-result.gsub!(/^Valid CSS information.*/, '')
+result.gsub!(/^Valid CSS information.*/m, '')
 
 puts result
