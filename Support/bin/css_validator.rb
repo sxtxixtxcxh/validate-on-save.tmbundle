92237cd195f127c534901930f6b12d0e2492d7c8
M. Auayan <sxtxixtxcxh@gmail.com>
2009-11-08 06:09:56 -0800

    absolute path to CSS validator jar

--- Support/bin/css_validator.rb
+++ Support/bin/css_validator.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 FILEPATH = ENV['TM_FILEPATH']
-result = `java -jar css/css-validator.jar file:#{FILEPATH} 2>&1`
+SUPPORT  = ENV['TM_BUNDLE_SUPPORT']
+result = `java -jar #{SUPPORT.gsub(' ','\ ')}/bin/css/css-validator.jar file:#{FILEPATH} 2>&1`
 
 puts result
