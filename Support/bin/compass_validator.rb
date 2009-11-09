58f5e8bd5c7ed2aaaf695489f75c99887ca55439
M. Auayan <sxtxixtxcxh@gmail.com>
2009-11-08 18:24:30 -0800

    fixed compass root directory detection

--- Support/bin/compass_validator.rb
+++ Support/bin/compass_validator.rb
@@ -1,20 +1,22 @@
 #!/usr/bin/env ruby
 FILEPATH = ENV['TM_FILEPATH']
 PATHS = File.dirname(FILEPATH).split('/');
+compass_root = FILEPATH.split("/app/")[0]
+
 dir = ''
 PATHS.reverse.each do |path|
   dir = PATHS[0,PATHS.index(path)+1].join('/')
   unless dir.empty?
     Dir.chdir( dir )
     unless( Dir.glob('config.rb').empty? )
-      COMPASS_ROOT = dir
+      compass_root = dir
       break
     end
   end
 end
 
-if defined? COMPASS_ROOT || COMPASS_ROOT = FILEPATH.split("/app/")[0]
-  result = `compass --update #{COMPASS_ROOT.gsub(' ','\ ')} 2>&1`
+if compass_root
+  result = `compass --update #{compass_root.gsub(' ','\ ')} 2>&1`
 else
   result = "Could not automatically determine your Compass project root."
 end
