6e157a501a0d750e26bc4d55daeb8aa181d6d09b
M. Auayan <sxtxixtxcxh@gmail.com>
2009-11-16 14:19:59 -0800

    Renamed Validate Compass to Validate Sass. Updated Compass validator to fallback to sass syntax checking. Should now work for non-compass based sass files.

--- Support/bin/compass_validator.rb
+++ Support/bin/compass_validator.rb
@@ -2,7 +2,7 @@
 FILEPATH = ENV["TM_FILEPATH"]
 PATHS = File.dirname(FILEPATH).split('/');
 COMPASS_BIN = ENV["TM_COMPASS"] ||= "compass"
-compass_root = FILEPATH.split("/app/")[0] || FILEPATH.split("/public/")[0]
+compass_root = (split = FILEPATH.split("/app/")).size > 1 ? split[0] : false
 
 PATHS.reverse.each do |path|
   dir = PATHS[0,PATHS.index(path)+1].join('/')
@@ -18,7 +18,7 @@ end
 if compass_root
   result = `"#{COMPASS_BIN}" --update #{compass_root.gsub(' ','\ ')} 2>&1`
 else
-  result = "Could not automatically determine your Compass project root."
+  result = `sass -c "#{FILEPATH}" 2>&1`
 end
 
 puts result
