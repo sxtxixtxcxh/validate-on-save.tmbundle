ab15fe68ab9884be98010729305a43b180855a84
Jim Myhrberg <contact@jimeh.me>
2010-01-08 22:22:03 +0200

    updated compass/sass validator again, since me and sxtxixtxcxh apparently don't speak the same english language... ^_^

--- Support/bin/compass_validator.rb
+++ Support/bin/compass_validator.rb
@@ -2,17 +2,20 @@
 filepath = ENV["TM_FILEPATH"]
 paths = File.dirname(filepath).split('/');
 
-if ENV["TM_COMPASS"].nil?
-  sass_bin = ENV["TM_SASS"] ||= "sass"
-  puts `"#{sass_bin}" -c "#{filepath}" 2>&1`
-else
+if ENV["TM_COMPASS"] != "false"
   compass_bin = ENV["TM_COMPASS"] ||= "compass"
   compass_root = (split = filepath.split("/app/")).size > 1 ? split[0] : false
   paths.reverse.each do |path|
     dir = paths[0,paths.index(path)+1].join('/')
     if !dir.empty? && File.exist?(File.join(dir, 'config.rb'))
-        compass_root = dir and break
+      compass_root = dir and break
     end
   end
+end
+
+if compass_root
   puts `"#{compass_bin}" --update #{compass_root.gsub(' ','\ ')} 2>&1`
+else
+  sass_bin = ENV["TM_SASS"] ||= "sass"
+  puts `"#{sass_bin}" -c "#{filepath}" 2>&1`
 end
