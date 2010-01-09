add7639aee26e2d88b13a30940490b55d6d0bb6c
Jim Myhrberg <contact@jimeh.me>
2010-01-09 09:18:32 +0200

    fixed the (stupid) trim issue, trimming and validation now works

--- Support/lib/validate_on_save/trim.rb
+++ Support/lib/validate_on_save/trim.rb
@@ -1,6 +1,6 @@
 class VOS
   def self.trim
-    input = File.read(ENV["TM_SELECTED_FILE"])
+    input = STDIN.read
     input.gsub!(/(.*?)(\S+)[\t ]+$/s, "\\1\\2") if opt("VOS_TRIM_LINES")
     puts input
   end
